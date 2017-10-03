439815ea6265af5eac7dee074f613b860d676bd6
Evgeny Krutko <e.s.krutko@gmail.com>
2017-10-03 20:35:12 +0300

    tmux and ssh-agent

--- buildStuff.sh
+++ buildStuff.sh
@@ -1,11 +1,15 @@
 export arch=mingw-w64-i686
 pacman -S --needed \
 	git \
+	git-flow \
+	tig \
 	base-devel \
 	zlib \
+	tmux \
 	$arch-toolchain \
 	$arch-cmake \
 	$arch-boost \
 	$arch-qt5 \
+	$arch-qt-creator \
 	$arch-python2 \
 

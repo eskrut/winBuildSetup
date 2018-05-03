1a8f61f62a0ace2331e59c488bbcc936c4d4d10f
Evgeny Krutko <e.s.krutko@gmail.com>
2018-05-03 12:59:37 +0300

    update

--- qthtSoft.sh
+++ qthtSoft.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 export PATH=/c/build/htData:/c/doli/dope/DoPE:/c/qwt/lib:/c/build/libfem:/c/local/fann/bin:$PATH
 
-#export PATH=/c/local/vtkd/bin:$PATH
+export PATH=/c/local/vtkd/bin:$PATH
 
 qtcreator &
--- setPathForhtSoft.sh
+++ setPathForhtSoft.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 export PATH=/c/build/htData:/c/doli/dope/DoPE:/c/qwt/lib:/c/build/libfem:/c/local/fann/bin:$PATH
 
 #export PATH=/c/local/vtkd/bin:$PATH
